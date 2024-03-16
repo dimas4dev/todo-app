d544192c6bc035ab3964d05f89e560c632e9d263
Dimas Mendoza <damendoza98@gmail.com>
2024-03-15 23:53:44 -0500

    Agrega componentes de tarea a la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Task } from "./components/Task";
 import { SectionContainer } from "./components/core/Container";
 import { MainCard } from "./components/core/MainCard";
 
@@ -5,7 +6,11 @@ function App() {
   return (
     <SectionContainer>
       <MainCard>
-        <h1>Hello, World!</h1>
+        <Task task='Task 1' onRemove={() => console.log('Delete 1')} onComplete={() => console.log('Complete 1')} />
+        <Task task='Task 1' onRemove={() => console.log('Delete 1')} onComplete={() => console.log('Complete 1')} />
+        <Task task='Task 1' onRemove={() => console.log('Delete 1')} onComplete={() => console.log('Complete 1')} />
+        <Task task='Task 1' onRemove={() => console.log('Delete 1')} onComplete={() => console.log('Complete 1')} />
+
       </MainCard>
     </SectionContainer>
   );
---
+++ src/components/Task.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+import { IconTrash } from './core/IconTrash';
+import { IconDone } from './core/IconDone';
+
+const TaskContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  width: 95%;
+  background-color: ${({ theme }) => theme.light.colors.background};
+  border-radius: 8px;
+  margin: 1rem;
+`;
+
+const TaskText = styled.p`
+  color: ${({ theme }) => theme.light.colors.text};
+  font-size: 1rem;
+  font-weight: ${({ theme }) => theme.typography.fontWeight.regular};
+`;
+
+const TaskButton = styled.button`
+  background-color: transparent;
+  border: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const IconDiv = styled.div`
+  display: flex;
+  gap: 12px;
+  justify-content: space-between;
+  align-items: center;
+  `;
+
+interface TaskProps {
+    task: string;
+    onRemove: () => void;
+    onComplete: () => void;
+}
+
+export const Task = ({ task, onRemove, onComplete }: TaskProps) => {
+    return (
+        <TaskContainer>
+            <TaskText>{task}</TaskText>
+            <IconDiv>
+                <TaskButton title='completeTask' onClick={onComplete}>
+                    <IconDone />
+                </TaskButton>
+                <TaskButton title='removeTask' onClick={onRemove}>
+                    <IconTrash />
+                </TaskButton>
+            </IconDiv>
+        </TaskContainer>
+    );
+}
