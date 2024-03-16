69b60415ed42d0afc39ef78bec8c897d86ed61e1
Dimas Mendoza <damendoza98@gmail.com>
2024-03-16 11:39:47 -0500

    Agrega Provider de Redux al componente principal

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
+import { Provider } from "react-redux";
 
 import App from './App.tsx'
+import { store } from "./redux/store";
 import theme from './styles/theme.ts'
 
 import './index.css'
@@ -10,9 +12,11 @@ import ResetStyles from './styles/resets.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <ResetStyles />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <ResetStyles />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 )
